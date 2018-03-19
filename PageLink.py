d34481f884006a0d9cb6a379af716afee1ffd9a1
Daniela Kaun <danielak@famkaun.local>
2018-03-19 16:53:29 +0100

    first version of triple for the resulting file

--- PageLink.py
+++ PageLink.py
@@ -1,51 +1,53 @@
-import xml.etree.ElementTree as ET
 import re
 import requests
 from bs4 import BeautifulSoup
 
-triple_element = []
-ttl_triple =[]
+triple_object = []
+ttl_triple = []
 
+# open file of wikipedia article and extract sentence
+def extract_sentence(triple_object):
+    with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/Cyrano_de_Bergerac_modified.xml',
+              encoding='cp65001') as wiki_f:
+        # ENCODING: cp437, cp850, cp852, cp858, cp1250, cp65001 !, latin_1, iso8859_2, iso8859_15
+        wiki_file = wiki_f.read()
+        re_match = re.search(r'([^.]*\[\[' + triple_object + '\]\][^.]*\.)', wiki_file,
+                            re.IGNORECASE)  # TODO adapting regex to sentence strucutre
+        sentence = re_match.group()
+        print('### Sentence with containing object: \n{}'.format(sentence))
 
+# open file of links
 with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/Cyrano_links.ttl') as ttl_f:
     ttl_file = ttl_f.read()
-    #print(ttl_file)
-    #print(f.readlines())
-    #spliting each triples of ttl file in subj, pred, obj
+    # print(ttl_file)
+    # print(f.readlines())
+
+    # spliting each triples of ttl file in subj, pred, obj
     ttl_triple = ttl_file.split(" ")
-    print('1' + ttl_triple[0])
-    print('2' + ttl_triple[1])
-    print('3' + ttl_triple[2])
+    # print('1' + ttl_triple[0])     print('2' + ttl_triple[1])     print('3' + ttl_triple[2])
 
-    #extracting the object for further research - object to extract the sentence from wikipedia
-    triple_element = ttl_triple[2].split('/')
-    print(triple_element[4][:-1]) #tripe without http content
+    # extracting the object and extracting sentence from file
+    i = 2
+    # for ttl_triple in ttl_file:
+    while i <= 11:
+        print('### Iterating through ttf_file with index: {}'.format(i))
+        triple_object = ttl_triple[i].split('/')[4][:-1]
+        print('### Tripple Object: \n{}'.format(triple_object))
+        extract_sentence(triple_object)
+        i += 3
 
-#extracting sentence from wikipedia
-url = "https://en.wikipedia.org/wiki/Cyrano_de_Bergerac"
-response = requests.get(url)
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-content_div = soup.find(id="mw-content-text").find(class_="mw-parser-output")
-for element in content_div.find_all('p', recursive=False):
-    for child_element in element.find_all(title='Sannois', recursive=False, href=True):
-        print(element)
-        #element = str(element)
-        re_sent = re.match('some', "something") #answer: <_sre.SRE_Match object; span=(0, 4), match='some'>
-        print(re_sent)
-        #print(re.match(r"[^.]*Sannois[^.]*\.", str(element))) #alternative no regex, split(".") + if 'sannois' in element; RE
+# extracting sentence from wikipedia
+def extract_wiki(url):
+    # url = "https://en.wikipedia.org/wiki/Cyrano_de_Bergerac"
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    content_div = soup.find(id="mw-content-text").find(class_="mw-parser-output")
+    for element in content_div.find_all('p', recursive=False):
+        for child_element in element.find_all(title='Sannois', recursive=False, href=True):
+            print(element)
+            print(re.search(r"[^.]*Sannois[^.]*\.",
+                            str(element)))  # alternative no regex, split(".") + if 'sannois' in element; RE
+# extract_wiki("https://en.wikipedia.org/wiki/Cyrano_de_Bergerac")
 
-
-#fileparsing
- with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/Cyrano_de_Bergerac.xml') as wiki_f:
-     wiki_file = wiki_f.read()
-     print(wiki_file)
-
-
-#et
-# tree = ET.parse('C:/Users/danielak/Desktop/country_data.xml')
-#                 #'Dokumente Daniela/UNI/FIZ/First Task/Cyrano_de_Bergerac.xml')
-# print(tree)
-
-
-#creating file
+# TODO creating file
