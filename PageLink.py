7d3f713bac0ba46032c150410b3fc1aadf893f11
Daniela Kaun <danielak@famkaun.local>
2018-04-28 18:29:00 +0200

    first draft for all wikipage (included to find right wikipage)

--- PageLink.py
+++ PageLink.py
@@ -1,6 +1,9 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import time
+
+start = time.time()
 
 def open_wiki_file():
     with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/Cyrano_de_Bergerac.xml',
@@ -41,13 +44,13 @@ def open_wiki_file():
         wiki_file = wiki_file.replace("&quot;", "\"")
         return wiki_file
 
-
 # open file of wikipedia article and extract sentence
 def extract_sentence(triple_object, wiki_file):
     # enhance_entity
     triple_object = triple_object.replace('_', ' ')
     triple_object = triple_object.replace('(', '\(')
-    triple_object = triple_object.replace(')', '\)') #TODO add & replacing with &amp;
+    triple_object = triple_object.replace(')', '\)')
+    triple_object = triple_object.replace('&', '&amp;')
 
     re_match = re.search(r'([^.]*\[\[' + triple_object + '[^.]*\.)', wiki_file,  # (\#.+)?(\|.+)?\]\]
                          re.IGNORECASE)
@@ -65,48 +68,32 @@ file = open("file_result_split_period_advanced1904.txt", "w+", encoding='cp65001
 
 
 # write triple to file 'file_result.txt
-def write_file(ttl_triple, sentence, i):
-    file.write(ttl_triple[i - 2] + ' ' + ttl_triple[i] + ' \"' + sentence + '\"')
+def write_file(ttl_triple, sentence):
+    file.write(ttl_triple[0] + ' ' + ttl_triple[2] + ' \"' + sentence + '\" \n')
     print('### Successfully wrote to File')
 
 
 # open file of links
 with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/Cyrano_links.ttl',
           encoding='cp65001') as ttl_f:
-    ttl_file = ttl_f.read()
-
-    # spliting each triples of ttl file in subj, pred, obj
-    ttl_triple = ttl_file.split(" ")
+    ttl_file = ttl_f.readlines()
     # get wikifile
     from_wiki_file = open_wiki_file()
-
     # extracting the object and extracting sentence from file
-    i = 2
     for line in ttl_file:
-        print('### Iterating through ttf_file with index: {}'.format(i))
-        triple_object = ttl_triple[i].split('/')[4][:-1]
+        print('### LINE: {} '.format(line))
+        ttl_triple = line.split(" ")    # spliting each triples of ttl file in subj, pred, obj
+        triple_object = ttl_triple[2].split('/')[4][:-1]
         print('### Tripple Object: \n{}'.format(triple_object))
         if triple_object:
             sentence = extract_sentence(triple_object, from_wiki_file)
         else:
             print("No Triple Object")
         if sentence:
-            write_file(ttl_triple, sentence, i)
+            write_file(ttl_triple, sentence)
         else:
             print('No sentence')
-        i += 3
+        #i += 3
 
-
-# NOT USING: extracting sentence from wikipedia
-def extract_wiki(url):
-    # url = "https://en.wikipedia.org/wiki/Cyrano_de_Bergerac"
-    response = requests.get(url)
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    content_div = soup.find(id="mw-content-text").find(class_="mw-parser-output")
-    for element in content_div.find_all('p', recursive=False):
-        for child_element in element.find_all(title='Sannois', recursive=False, href=True):
-            print(element)
-            print(re.search(r"[^.]*Sannois[^.]*\.",
-                            str(element)))  # alternative no regex, split(".") + if 'sannois' in element; RE
-# extract_wiki("https://en.wikipedia.org/wiki/Cyrano_de_Bergerac")
+end = time.time()
+print('### TIME {}'.format(end-start))
