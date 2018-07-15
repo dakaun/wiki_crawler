ea63bf1a42859b9c0b52cb111130666fa0001a75
daniela <daniela.kaun@gmx.de>
2018-07-15 22:42:38 +0200

    adapt sumWiki_server to file 1

--- wikiextractor/wikifile_treat/sumWiki_server.py
+++ wikiextractor/wikifile_treat/sumWiki_server.py
@@ -1,9 +1,9 @@
 import shutil
 import os
 
-ROOTDIR = '/home/daniela/wikipedia20180401/wikiextractor/result_wikiextractor_1_links/'
+ROOTDIR = '/home/daniela/wikipedia20180401/wikiextractor/result_wikipart_5/result_wikiextractor_1/'
 
-complete_wiki = open('/home/daniela/wikipedia20180401/wikiextractor/result_wikiextractor_1_links/wiki_1.txt', 'wb')
+complete_wiki = open('/home/daniela/wikipedia20180401/wikiextractor/result_wikipart_5/result_wikiextractor_1/wiki_1.txt', 'wb')
 for root, dirs, files in os.walk(ROOTDIR):
     print('---root {}'.format(root))
     for tempfile in files:
