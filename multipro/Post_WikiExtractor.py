f54036633e363dfc33b96794e61ae891a165dcfe
daniela <daniela.kaun@gmx.de>
2018-08-20 14:23:46 +0200

    insert necessary scripts in dir - need to be completed; test Post_Wiki

---
+++ multipro/Post_WikiExtractor.py
@@ -0,0 +1,20 @@
+import shutil
+import os
+
+# After the processing with WikiExtractor.py the articles are in several subdirectories and subfiles with the following structure
+# TODO test if rootdir with result_wikipart_5 is enough to sum up files
+
+ROOTDIR = '/home/daniela/wikipedia20180401/wikiextractor/test_wikipart_5/'
+PATH_COMPLETE_WIKI = '/home/daniela/wikipedia20180401/wikiextractor/test_wikipart_5/'
+
+os.makedirs(os.path.dirname(PATH_COMPLETE_WIKI), exist_ok=True)
+complete_wiki = open(PATH_COMPLETE_WIKI + 'wiki_1+2.txt', 'wb')
+for root, dirs, files in os.walk(ROOTDIR):
+    print('---root {}'.format(root))
+    for tempfile in files:
+        print('---file {}'.format(tempfile))
+        tempfile_dir = os.path.join(root, tempfile)
+        open_tempfile = open(tempfile_dir, 'rb')
+        shutil.copyfileobj(open_tempfile, complete_wiki)
+
+print('--finished with all files')
--- multipro/Semantic_WikiLinks.py
+++ multipro/Semantic_WikiLinks.py
@@ -1,19 +1,36 @@
 from multipro import Preprocessing
 from wikiextractor import WikiExtractor
 
+# MAIN FILE TO CONNECT ALL PROCESS - IF POSSIBLE
+
 # import initial wikidump and do preprocessing
-with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/wiki_dump/wiki_dump.txt',
-              encoding='cp65001') as wiki_dump:
-    # number of subfiles of wikidump to create
-    NB_OF_SUBFILES = 2
-    # path to save created subfiles of wikidump
-    FILEPATH = 'C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/wiki_dump/sub_files/'
-    # Preprocessing,
-    Preprocessing.pre_process(wiki_dump, NB_OF_SUBFILES, FILEPATH)
-
-
+# with open('C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/wiki_dump/wiki_dump.txt',
+#               encoding='cp65001') as wiki_dump:
+#     # number of subfiles of wikidump to create
+#     NB_OF_SUBFILES = 2
+#     # path to save created subfiles of wikidump
+#     FILEPATH = 'C:/Users/danielak/Desktop/Dokumente Daniela/UNI/FIZ/First Task/wiki_dump/sub_files/'
+#     # Preprocessing
+#     Preprocessing.pre_process(wiki_dump, NB_OF_SUBFILES, FILEPATH)
 
 # run WikiExtractor on each files
+
+
+# import shutil
+# import os
+#
+# ROOTDIR = '/home/daniela/wikipedia20180401/wikiextraction/result_wikipart_5/result_wikiextractor_5/'
+#
+# complete_wiki = open('/home/daniela/wikipedia20180401/wikiextraction/result_wikipart_5/result_wikiextractor_5/wiki_5.txt', 'wb')
+# for root, dirs, files in os.walk(ROOTDIR):
+#     print('---root {}'.format(root))
+#     for tempfile in files:
+#         print('---file {}'.format(tempfile))
+#         tempfile_dir = os.path.join(root, tempfile)
+#         open_tempfile = open(tempfile_dir, 'rb')
+#         shutil.copyfileobj(open_tempfile, complete_wiki)
+#
+# print('--finished with all files')
 # summing up result files of WikiExtractor to one file
 # run script to extract sentences
 
