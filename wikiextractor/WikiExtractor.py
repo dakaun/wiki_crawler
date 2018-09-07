6bcbc6fe10fada07e7f3aba4d2a49db96bd9ff49
daniela <daniela.kaun@gmx.de>
2018-09-07 10:10:38 +0200

    add print

--- wikiextractor/WikiExtractor.py
+++ wikiextractor/WikiExtractor.py
@@ -2849,13 +2849,16 @@ def process_dump(input_file, template_file, out_file, file_size, file_compress,
         if not m:
             continue
         tag = m.group(2)
+        print('here')
         if tag == 'base':
             # discover urlbase from the xml dump file
             # /mediawiki/siteinfo/base
             base = m.group(3)
             options.urlbase = base[:base.rfind("/")]
+            print('here')
         elif tag == 'namespace':
             mk = keyRE.search(line)
+            print('here')
             if mk:
                 nsid = mk.group(1)
             else:
