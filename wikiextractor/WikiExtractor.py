20bafb796859a350958f08d7fdcc295f9c4fd9fd
daniela <daniela.kaun@gmx.de>
2018-09-07 10:22:18 +0200

    bug fix

--- wikiextractor/WikiExtractor.py
+++ wikiextractor/WikiExtractor.py
@@ -2849,16 +2849,16 @@ def process_dump(input_file, template_file, out_file, file_size, file_compress,
         if not m:
             continue
         tag = m.group(2)
-        print('here')
+        print('here1')
         if tag == 'base':
             # discover urlbase from the xml dump file
             # /mediawiki/siteinfo/base
             base = m.group(3)
             options.urlbase = base[:base.rfind("/")]
-            print('here')
+            print('here2')
         elif tag == 'namespace':
             mk = keyRE.search(line)
-            print('here')
+            print('here3')
             if mk:
                 nsid = mk.group(1)
             else:
