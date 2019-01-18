a11b4ece4b00d66ba3765d38b6e2d4310db9fd0a
Daniela Kaun <ugebn@student.kit.edu>
2019-01-18 10:34:59 +0100

    bug fix

--- Extract_Sentences.py
+++ Extract_Sentences.py
@@ -110,7 +110,7 @@ def result_file(path):
     sentence_tokenizer._params.abbrev_types.update(extra_abbreviation)
 
     now = datetime.datetime.now()
-    resulting_file= open(resulting_path+ '/res' + str(now.month) + str(now.day) + '.txt', "w+") #, encoding='cp65001'
+    #resulting_file= open(resulting_path+ '/res' + str(now.month) + str(now.day) + '.txt', "w+") #, encoding='cp65001'
     df = pd.DataFrame(columns={'article_title', 'link', 'sentence'})
 
     articles = open_wiki_files(INPUT_PATH)
